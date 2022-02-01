17f583ebcd6c8d04eb689b9a2dd39ec4082c9970
Mukhlis Akbarrudin <mukhlis3389@gmail.com>
2022-02-02 03:43:56 +0700

    feat: add basic homepage

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -12,7 +12,7 @@ export function openGraph({
   templateTitle,
   description,
   // Or, you can use my server with your own logo.
-  logo = 'https://bukan.dev/favicon/large-og.png',
+  logo = 'https://bukan.dev/images/logo.png',
 }: OpenGraphType): string {
   const ogLogo = encodeURIComponent(logo);
   const ogSiteName = encodeURIComponent(siteName.trim());
