8876169bfb533de357f3ab70a5128aba0f702b6f
Mukhlis Akbarrudin <mukhlis3389@gmail.com>
2022-02-02 19:43:56 +0700

    Revert "fix: use default layout in NextImage"
    This reverts commit 6ca2bad20a846c0b0de8f06306d41542096720ea.

--- src/components/NextImage.tsx
+++ src/components/NextImage.tsx
@@ -51,6 +51,7 @@ export default function NextImage({
         height={height}
         alt={alt}
         onLoadingComplete={() => setStatus('complete')}
+        layout='responsive'
         {...rest}
       />
     </figure>
